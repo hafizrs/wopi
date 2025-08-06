37470f6102bec53fb197ee57722601f1e831df67
MD. Hafizur Rahman <hafizur.rahman@selisegroup.com>
2025-08-06 13:38:36 +0600

    remove

--- src/Contracts/Infrastructure/IAppSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace Selise.Ecap.SC.WopiMonitor.Contracts.Infrastructure
-{
-    public interface IAppSettings
-    {
-        string ServiceName { get; set; }
-        string BlocksAuditLogQueueName { get; set; }
-        string CollaboraBaseUrl { get; set; }
-    }
-} 
--- src/Contracts/Infrastructure/IServiceClient.cs
+++
@@ -1,9 +0,0 @@
-using SeliseBlocks.Genesis.Framework.Infrastructure;
-
-namespace Selise.Ecap.SC.WopiMonitor.Contracts.Infrastructure
-{
-    public interface IServiceClient
-    {
-        T SendToQueue<T>(string queueName, object message);
-    }
-} 
